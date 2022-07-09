878eae160d496d21ae033a7ba75316aff2962d5f
Uszczi <mateuszpapuga24@gmail.com>
2022-07-09 18:28:11 +0200

    add typescript

---
+++ src/api/movies.ts
@@ -0,0 +1,7 @@
+export interface Movie {
+  productionYear: Date;
+  title: string;
+  watchDate: Date;
+  Director: string; // TODO this should be a list of directors
+  id: number; // TODO this should be a string
+}
